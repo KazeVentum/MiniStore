73040318d90800fe556a35966f0e2aa2c3af18ba
KazeVentum <kazeventum@gmail.com>
2025-12-01 18:48:39 -0500

    feat: :bug: Added new scrips for windows

---
+++ setup.bat
@@ -0,0 +1,6 @@
+@echo off
+echo 💖 Instalando MiniStore... Por favor espera...
+call npm install
+call npm run install-all
+echo ✨ ¡Instalación completada! Ahora puedes usar 'start.bat' para abrir la app.
+pause
---
+++ start.bat
@@ -0,0 +1,3 @@
+@echo off
+echo 🚀 Iniciando MiniStore...
+npm start
